829fe4af9be14fbd8d0a2df3ae98c52d3e407947
king-open <kingwang.0624@gmail.com>
2023-11-16 05:39:04 +0800

    完成第一个页面的样式

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -23,21 +23,23 @@ export const WelcomeLayout: React.FC = () => {
     config: { duration: 300 }
   })
   return (
-    <div>
-      <header>
-        <img src={logo} />
-        <h1>starsMoney</h1>
+    <div className='bg-#5f43bf' h-screen flex flex-col items-stretch pb-16px>
+      <header shrink-0 text-center pt-64px>
+        <img src={logo} w-64px />
+        <h1 text="#D4D4EE" text-32px>starsMoney</h1>
       </header>
-      <main>
+      <main shrink-1 grow-1 bg-white m-16px rounded-8px flex justify-center >
         {transitions((style, pathname) =>
           <animated.div key={pathname} style={style}>
             {map.current[pathname]}
           </animated.div>
         )}
       </main>
-      <footer>
-        <Link to={linkMap[location.pathname]}>下一页</Link>
-        <Link to="/welcome/xxx">跳过</Link>
+      <footer shrink-0 text-center text-24px text-white grid grid-cols-3 grid-row-1>
+        <Link style={{ gridArea: '1 / 2 / 2 / 3' }}
+         to={linkMap[location.pathname]}>下一页</Link>
+        <Link style={{ gridArea: '1 / 3 / 2 / 4' }} 
+         to="/welcome/xxx">跳过</Link>
       </footer>
     </div>
   )
