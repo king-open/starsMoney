f492c4c8d39999871e9337c1b7137361114a2c1f
king-open <kingwang.0624@gmail.com>
2023-11-20 08:06:38 +0800

    将 .w-icon 的代码放入 components layer

--- uno.config.ts
+++ uno.config.ts
@@ -9,6 +9,18 @@ export default defineConfig({
   shortcuts: {
   },
   safelist: [],
+  preflights: [
+    {
+      layer: 'components',
+      getCSS: () => `
+        .w-icon{
+          fill: currentColor;
+          width: 1.2em;
+          height: 1.2em;
+        }
+      `
+    },
+  ],
   presets: [
     presetUno(),
     presetAttributify(),
